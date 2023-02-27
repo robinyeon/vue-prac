94136d7118365a77e23765f431a4e20a9d8a4d69
robinyeon <robin.yeon@gmail.com>
2023-02-27 21:34:37 +0900

    Feat: basic layout

---
+++ src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Post v-for="(post, idx) in posts" :key="idx" :post="posts[idx]" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+  name: "container",
+  props: {
+    posts: Array,
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style></style>
